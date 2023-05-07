76cc185524ac62e6a0007ecfe0f932a5a16b456e
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-05-07 12:48:26 +0500

    calculation baqi hai

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+// declare area
 bodyofmodal = document.getElementsByClassName('bodyofmodal')[0];
 modal = document.getElementsByClassName('modal')[0];
+youwillbe = document.getElementsByClassName('youwillbe')[0];
 monthi = document.getElementById('monthi');
 dayi = document.getElementById('dayi');
 yeari = document.getElementById('yeari');
 button = document.getElementById("button");
+counter = document.getElementsByClassName('counter')[0];
+month = counter.children[0].children[0];
+days = counter.children[1].children[0];
+hours = counter.children[2].children[0];
+// seconds = counter.children[3].children[0];
 
+// adding listener to button
 button.addEventListener('click', ()=>{
 
     const ristrict = document.getElementById("ristrict");
@@ -19,10 +27,33 @@ button.addEventListener('click', ()=>{
     }
 
     else{
+        
+        let tempDate = new Date();
+
+        const inputdate = new Date(`${yeari.value}-${monthi.value}-${dayi.value}`);
+
+        // var msToday = tempDate.getTime();
+        // var msDoomsday = inputdate.getTime();
+
+        var msDiff =  tempDate.getTime() - inputdate.getTime();
+        console.log(msDiff);
+
+    
+        var months = ["Jan", "Feb", "Mar", "Apr","May", "Jun", "Jul", "Aug","Sep", "Oct", "Nov", "Dec"]
+
         modal.classList.add('animation');
         setTimeout(()=>{
             bodyofmodal.classList.add('none');
-        }, 1000)
+        }, 10)
+        youwillbe.innerText = `Your age will be ${(tempDate.getFullYear() - yeari.value) + (1)} on ${dayi.value}/${months[(monthi.value) - 1]}/${tempDate.getFullYear()}`
+
+        month.innerText = `${(msDiff / (1000 * 60 * 60 * 24 * 30))}`
+        
+        days.innerText = `${msDiff/ (1000 * 60 * 60 * 24)}`
+        console.log(dayi.value + " -> it is day")
+        console.log(monthi.value + " -> it is month")
+        console.log(yeari.value + " -> it is year")
+
     }
 
 })
